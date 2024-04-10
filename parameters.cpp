fc40442246decad162879d21dc2f27ba0bb4ad03
ktnkdoomer <egormoroz2020@gmail.com>
2024-04-10 13:30:55 +0300

    bench 8584966

--- parameters.cpp
+++ parameters.cpp
@@ -25,31 +25,31 @@ int move_overhead = defaults::move_overhead;
 #define PARAMETER(name, def, min, max, step) \
     Parameter& name = registry.add(Parameter(#name, def, min, max, step));
 
-PARAMETER(asp_init_delta, 11, 1, 100, 5)
+PARAMETER(asp_init_delta, 12, 1, 100, 5)
 PARAMETER(asp_min_depth, 8, 1, 12, 1)
-PARAMETER(lmr_coeff, 53, 40, 100, 5)
+PARAMETER(lmr_coeff, 48, 40, 100, 5)
 
 
-PARAMETER(nmp_min_depth, 2, 1, 10, 1)
+PARAMETER(nmp_min_depth, 3, 1, 10, 1)
 PARAMETER(nmp_base, 4, 2, 4, 1)
 PARAMETER(nmp_depth_div, 8, 4, 8, 1)
-PARAMETER(nmp_eval_div, 86, 80, 200, 20)
+PARAMETER(nmp_eval_div, 116, 80, 200, 20)
 
 PARAMETER(iir_min_depth, 2, 2, 8, 2)
 
 PARAMETER(rfp_max_depth, 7, 2, 8, 2)
-PARAMETER(rfp_margin, 134, 50, 250, 25)
+PARAMETER(rfp_margin, 119, 50, 250, 25)
 
 PARAMETER(rz_max_depth, 6, 2, 8, 2)
-PARAMETER(rz_margin, 267, 100, 300, 50)
+PARAMETER(rz_margin, 239, 100, 300, 50)
 
 PARAMETER(sing_min_depth, 8, 6, 12, 1)
 
-PARAMETER(lmr_hist_div, 4590, 2048, 12288, 512)
+PARAMETER(lmr_hist_div, 4903, 2048, 12288, 512)
 
-PARAMETER(delta_margin, 353, 100, 400, 50)
+PARAMETER(delta_margin, 389, 100, 400, 50)
 
-PARAMETER(seefp_depth, 6, 2, 12, 1)
+PARAMETER(seefp_depth, 5, 2, 12, 1)
 
 }
 
